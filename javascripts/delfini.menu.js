e8be86868ee1a8ccd4acb43df52ba40ad3a155b1
Oliver Spryn <4b394b0f@opayq.com>
2012-05-31 04:36:44 +0000

    The menu component is now fully functional in this revision. The added enhancements include: [1] more consistency for cross-browser display, [2] auto-detection and correction for when the menu will spill off the bottom of the screen, [3] life-cycle and user interaction events, [4] captures user input for submission in a form, [5] selecting an item or click outside the menu closes it, and [6] a maximum height is now enforced on the menu.

--- javascripts/delfini.menu.js
+++ javascripts/delfini.menu.js
@@ -1,39 +1,149 @@
 /*
-Requires jQuery!
-
-Example usage:
-
-<ul class="dropdown">
-<li>Option</li>
-<li>Option</li>
-<li class="selected">Option</li>
-<li>A longer option</li>
-</ul>
+ * Delfini UI - Drop Down Menu
+ * -------------------------------
+ *
+ * ForwardFour Innovations: http://forwardfour.com/
+ * Delfini UI: http://delfiniui.com/
+ * 
+ * LICENSE
+ * 
+ * By viewing, using, or actively developing this framework in any way, you are
+ * henceforth bound the license agreement, and all of its changes, set forth by
+ * ForwardFour Innovations or the Delfini UI Group. The license can be found,
+ * in its entirety, at this address: http://delfiniui.com/license.
+ * 
+ * Copyright (c) 2012 and Onwards, ForwardFour Innovations & The Delfini UI Group
+ * http://delfiniui.com/license [Proprietary]
+ *
+ *
+ *
+ * Example usage:
+ *
+ * <ul class="dropdown" data-name="menu">      The "dropdown" class will activate this UI widget for a given
+ * <li>Option</li>                             unordered list
+ * <li>Option</li>                             The "data-name" attribute will be the name of the HTML element
+ * <li class="selected">Option</li>            when the data is submitted in a <form>
+ * <li>A longer option</li>                    The "selected" class shows the currently selected option
+ * </ul>
+ *
+ *
+ *
+ * Events dispatched:
+ *  - dropdownReady                            The unordered list has been properly modified and is ready for use
+ *  - menuOpened                               The user has opened the menu
+ *  - menuClosed [FORTH COMMING]               The user has closed the menu
+ *  - menuItemSelected                         A menu item has been selected
+ *  - newMenuItemSelected                      A different menu item than the previously selected item was chosen
 */
 
-$(document).ready(function() {
-	$.fn.delfini.menu = function() {
-		var menu = $(this);
+(function($) {
+	$(document).ready(function() {
+		var menus = $('ul.dropdown');
+		var menuItems = $('ul.dropdown li');
 		
-	//Resize the menu container so that it is automatically sized to the fit the longest menu option
-		menu.each(function() {
+	/**
+	 * Wrap the dropdown menu in a container div so that when it is floated with CSS when
+	 * it is opened, the contents around it do not collapse. Also, create a hidden input
+	 * element to store the value of the drop down component, using the <ul> "data-name"
+	 * attribute to name the hidden input element
+	*/
+	
+		menus.wrap('<div class="dropdownWrapper" />').after(function() {
 			var currentMenu = $(this);
-			var maxWidth = 0;
 			
-			currentMenu.children().each(function() {
-				var currentWidth = $(this).width();
-				
-				if (currentWidth > maxWidth) {
-					maxWidth = currentWidth;
-				}
-			});
+		//Tell the application that this element has been modified and is ready to go!
+			$(this).trigger('dropdownReady');
 			
-			currentMenu.width(maxWidth);
+			return '<input name="' + currentMenu.attr('data-name') + '" type="hidden" value="' + currentMenu.children('li.selected').text() + '"/>';
 		});
 		
 	//Open the menu
-		menu.click(function() {
-			$(this).addClass('open');
+		menus.click(function() {
+			var currentMenu = $(this);
+			var left = currentMenu.offset().left;
+			var top = currentMenu.offset().top;
+			var windowHeight = $(window).height();
+			
+		//Simply add another CSS class and the menu will open
+			currentMenu.addClass('open');
+			
+		/**
+		 * It will be necessary to float the menu to avoid pushing around other items
+		 * as it opens
+		 *
+		 * This process takes place after the open class has been applied so that the 
+		 * height of the menu can be calculated. The statement below determines whether 
+		 * or not the menu will spill over the bottom side of the screen when opened.
+		*/
+			if (top + currentMenu.height() > windowHeight) {
+				top -= (top + currentMenu.height() + 20) - windowHeight; //+20 easily makes up for the added height from the shadow and border
+			}
+			
+			currentMenu.css({
+				'left' : left + 'px',
+				'position' : 'absolute',
+				'top' : top + 'px'
+			});
+			
+		//Tell the application that this component has been opened
+			currentMenu.trigger('menuOpened');
 		});
-	};
-});
+		
+	/**
+	 * If something outside of the menu is clicked on, collapse the menu
+	 *
+	 * Since this event is trigged by clicking anywhere on the webpage,
+	 * and not by a specific element like many of the other events that
+	 * power this UI component, there is no event that is dispatched 
+	 * since we cannot refer to a specific dropdown menu without doing the
+	 * following memory intensive process on each mouse click:
+	 *  - select all open menus on the page
+	 *  - loop through each of these menus
+	 *  - check if the clicked element was any one of the open menus
+	 *  - if the clicked object was a menu, dispatch a close event and 
+	 *    close the menu
+	*/
+		$(document).click(function(e) {
+			if (!$(e.target).is('ul.dropdown') && !$(e.target).parents().is('ul.dropdown')) {
+			//Close all open menus
+				menus.removeClass('open').removeAttr('style');
+			}
+		});
+		
+	//Select an item from the menu
+		menuItems.click(function(e) {
+			var currentItem = $(this);
+			var menu = currentItem.parent();
+			var itemNew = true;
+			
+		//Deselect all other list items and select the clicked item
+			if (menu.hasClass('open')) {
+			//Check and see if the selected item was the same as the previously selected item
+				if (currentItem.parent().children('li.selected').is(currentItem)) {
+					itemNew = false;
+				}
+				
+				currentItem.addClass('reservedItemCurrentlySelcted').siblings('li.selected').removeClass('selected').siblings('li.reservedItemCurrentlySelcted').removeClass('reservedItemCurrentlySelcted').addClass('selected');
+				
+			//Close the menu
+				menu.removeClass('open').removeAttr('style');
+				e.stopPropagation(); //This prevents a click event from immediately reopening the menu
+				
+			//Update the value of the hidden field
+				menu.siblings('input').val(currentItem.text());
+				
+			//Tell the application that this component has been closed
+			//This event will be added whenever it can also be added to the $(document) click handler above
+				//currentMenu.trigger('menuClosed');
+				
+			//Tell the application that an item was selected from this component
+				currentMenu.trigger('menuItemSelected');
+				
+			//Tell the application if a different item was selected
+				if (itemNew) {
+					currentMenu.trigger('newMenuItemSelected');
+				}
+			}
+		});
+	});
+})(jQuery);
